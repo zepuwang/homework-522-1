81ccaf02012289c9482a97d976f18e8177897711
zepuwang <zepu@seas.upenn.edu>
2023-01-24 22:12:10 -0500

    Update model.py

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -2,29 +2,29 @@ import numpy as np
 
 
 class LinearRegression:
+    """
+    A linear regression model that uses a closed form solution to fit the model.
+    """
 
     w: np.ndarray
     b: float
 
     def __init__(self):
-        # raise NotImplementedError()
         self.w = 0
         self.b = 0
 
-    def fit(self, X, y) -> None:
-        n = X.shape[0]
-        p = X.shape[1]
-        a = np.array([1] * n).reshape(n, 1)
-        x_ = np.concatenate((X, a), axis=1)
-        w_ = np.linalg.inv(x_.T @ x_) @ (x_.T @ y)
-        self.w = w_[:-1]
-        self.b = w_[-1]
-        # raise NotImplementedError()
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
 
-    def predict(self, X) -> np.ndarray:
-        y = np.dot(X, self.w)
-        # raise NotImplementedError()
-        return y
+        n = X.shape[0]
+        X = np.append(X, np.ones((n, 1)), axis=1)
+        y = y.reshape(n, 1)
+        self.w_ = np.linalg.inv(X.T @ X) @ (X.T @ y)
+
+    def predict(self, X: np.ndarray) -> np.ndarray:
+
+        n = X.shape[0]
+        X = np.append(X, np.ones((n, 1)), axis=1)
+        return X @ self.w_
 
 
 class GradientDescentLinearRegression(LinearRegression):
@@ -35,27 +35,30 @@ class GradientDescentLinearRegression(LinearRegression):
     def fit(
         self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
     ) -> None:
-        # raise NotImplementedError()
+        """
+        Fit the model for the given input using gradient descent.
+        Arguments:
+            X (np.ndarray): The input data.
+            y (np.ndarray): The input labels.
+        Returns:
+            np.ndarray: None.
+        """
         self.w = 0
         self.b = 0
-        self.n = len(X)
+        n = len(X)
         for i in range(epochs):
             y_pred = self.w * X + self.b
-            dm = (-2 / self.n) * sum(X * (y - y_pred))
-            db = (-1 / self.n) * sum(y - y_pred)
+            dm = (-2 / n) * sum(X * (y - y_pred))
+            db = (-1 / n) * sum(y - y_pred)
             self.w = self.w - dm * lr
             self.b = self.b - db * lr
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
         Predict the output for the given input.
-
         Arguments:
             X (np.ndarray): The input data.
-
         Returns:
             np.ndarray: The predicted output.
         """
         return self.w * X + self.b
-        return y_pred
-        # raise NotImplementedError()
