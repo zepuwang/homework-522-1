e68649697b4ce5f7632b3bfd79ed2ed46c968193
zepuwang <zepu@seas.upenn.edu>
2023-01-27 20:03:42 -0500

    Update model.py

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -14,17 +14,37 @@ class LinearRegression:
         self.b = 0
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Fit the model for the given input using a closed form solution.
 
+        Arguments:
+            X (np.ndarray): The input data.
+            y (np.ndarray): The input labels.
+
+        Returns:
+            np.ndarray: None.
+
+        """
         n = X.shape[0]
-        X = np.append(X, np.ones((n, 1)), axis=1)
+        X_ = np.append(X, np.ones((n, 1)), axis=1)  # Append column for bias
         y = y.reshape(n, 1)
-        self.w_ = np.linalg.inv(X.T @ X) @ (X.T @ y)
-        self.w = self.w_[:-1]
-    def predict(self, X: np.ndarray) -> np.ndarray:
+        self.w_ = np.linalg.inv(X_.T @ X_) @ (X_.T @ y)
+        self.w = self.w_[-1]
 
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict the output for the given input.
+
+        Arguments:
+            X (np.ndarray): The input data.
+
+        Returns:
+            np.ndarray: The predicted output.
+
+        """
         n = X.shape[0]
-        X = np.append(X, np.ones((n, 1)), axis=1)
-        return X @ self.w_
+        X_ = np.append(X_, np.ones((n, 1)), axis=1)
+        return np.dot(X, self.w_)
 
 
 class GradientDescentLinearRegression(LinearRegression):
@@ -37,11 +57,14 @@ class GradientDescentLinearRegression(LinearRegression):
     ) -> None:
         """
         Fit the model for the given input using gradient descent.
+
         Arguments:
             X (np.ndarray): The input data.
             y (np.ndarray): The input labels.
+
         Returns:
             np.ndarray: None.
+
         """
         self.w = 0
         self.b = 0
@@ -56,9 +79,12 @@ class GradientDescentLinearRegression(LinearRegression):
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
         Predict the output for the given input.
+
         Arguments:
             X (np.ndarray): The input data.
+
         Returns:
             np.ndarray: The predicted output.
+
         """
         return self.w * X + self.b
