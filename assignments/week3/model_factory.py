6687c56242aecc06c18b6065150eabc37a41022e
zepuwang <zepu@seas.upenn.edu>
2023-02-04 20:19:39 -0500

    Update model_factory.py

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -15,4 +15,6 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         MLP: The created model.
 
     """
-    return MLP(input_dim, 200, output_dim, 1, torch.nn.LeakyReLU, torch.nn.init.xavier_normal_)
+    return MLP(
+        input_dim, 200, output_dim, 1, torch.nn.LeakyReLU, torch.nn.init.xavier_normal_
+    )
