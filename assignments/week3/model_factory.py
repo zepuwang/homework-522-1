7926e2df93cf8d4ed68ccaf012aab2b68077f344
zepuwang <zepu@seas.upenn.edu>
2023-02-07 13:38:28 -0500

    Update model_factory.py

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -16,5 +16,5 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
 
     """
     return MLP(
-        input_dim, 200, output_dim, 1, torch.nn.GELU, torch.nn.init.xavier_normal_
+        input_dim, 512, output_dim, 3, torch.nn.GELU, torch.nn.init.xavier_normal_
     )
