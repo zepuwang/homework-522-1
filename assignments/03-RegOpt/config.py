b451e12d5f40f21ba5ca4e7de7f021b876b2ab61
zepuwang <zepu@seas.upenn.edu>
2023-02-14 20:55:34 -0500

    Update config.py

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -2,7 +2,7 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, Normalize, ToTensor
+from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
