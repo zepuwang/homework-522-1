47244c92c5e8d4e4812f69d6b7a1daa1627f121b
zepuwang <zepu@seas.upenn.edu>
2023-02-15 23:06:42 -0500

    Update config.py

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -8,7 +8,7 @@ from torchvision.transforms import Compose, ToTensor
 class CONFIG:
     batch_size = 128
     num_epochs = 20
-    initial_learning_rate = 0.001
+    initial_learning_rate = 0.003
     initial_weight_decay = 0
 
     lrs_kwargs = {
