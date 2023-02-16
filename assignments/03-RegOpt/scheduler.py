15a7612d0725a3026174fde7d74078dfd51b301b
zepuwang <zepu@seas.upenn.edu>
2023-02-15 22:35:06 -0500

    Update scheduler.py

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -9,40 +9,34 @@ class CustomLRScheduler(_LRScheduler):
     """
 
     def __init__(
-            self,
-            optimizer,
-            start_factor=1.0 / 3,
-            end_factor=1.0,
-            total_iters=5,
-            last_epoch=-1,
-            verbose=False,
-        ):
-            """
-            Create a new scheduler.
-            Note to students: You can change the arguments to this constructor,
-            if you need to add new parameters.
-            """
-            self.start_factor = start_factor
-            self.end_factor = end_factor
-            self.total_iters = total_iters
-            super().__init__(optimizer, last_epoch, verbose)
+        self,
+        optimizer,
+        start_factor=1.0 / 3,
+        end_factor=1.0,
+        total_iters=5,
+        last_epoch=-1,
+        verbose=False,
+    ):
+        """
+        Create a new scheduler.
+        Note to students: You can change the arguments to this constructor,
+        if you need to add new parameters.
+        """
+        self.start_factor = start_factor
+        self.end_factor = end_factor
+        self.total_iters = total_iters
+        super().__init__(optimizer, last_epoch, verbose)
 
     def get_lr(self) -> List[float]:
-            """
-            Learning rate schedule
-            Arguments:
-            None
-            Returns:
-                the scheduler
-            
-            """
-            if self.last_epoch < 1000:
-                return [
-               base_lr 
-                for base_lr in self.base_lrs
-            ]
-            
-            return [
-               base_lr * (0.99 ** self.last_epoch)
-                for base_lr in self.base_lrs
-            ]
+        """
+        Learning rate schedule
+        Arguments:
+        None
+        Returns:
+            the scheduler
+
+        """
+        if self.last_epoch < 1500:
+            return [base_lr for base_lr in self.base_lrs]
+
+        return [base_lr * (0.99**self.last_epoch) for base_lr in self.base_lrs]
