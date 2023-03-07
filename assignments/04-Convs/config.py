12e1c0c0a9eb655a797b81ceb0cb726dc86180a5
zepuwang <zepu@seas.upenn.edu>
2023-03-07 14:09:24 -0800

    Update config.py

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,11 +6,11 @@ from torchvision.transforms import Compose, ToTensor
 
 
 class CONFIG:
-    batch_size = 128
+    batch_size = 100
     num_epochs = 8
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1.61 * 1e-3)
+    ] = lambda model: torch.optim.Adam(model.parameters(), lr=1.6 * 1e-3)
 
     transforms = Compose([ToTensor()])
