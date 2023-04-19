723fda01d98874d248c2fb0402fb86aac2168445
zepuwang <zepu@seas.upenn.edu>
2023-04-19 18:45:03 -0400

    Update main.py

--- assignments/05-RL/main.py
+++ assignments/05-RL/main.py
@@ -30,11 +30,10 @@ for _ in range(100000):
         n = min(30, len(last_n_rewards))
         avg = sum(last_n_rewards[-n:]) / n
         improvement_emoji = "🔥" if (total_reward > avg) else "😢"
-        print(
-            f"{improvement_emoji} Finished with reward {int(total_reward)}.\tAverage of last {n}: {int(avg)}"
-        )
+
         if avg > 0:
-            print("🎉 Nice work! You're ready to submit the leaderboard! 🎉")
+            continue
+            # print("🎉 Nice work! You're ready to submit the leaderboard! 🎉")
         total_reward = 0
 
 env.close()
