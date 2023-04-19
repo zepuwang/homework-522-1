d06b35fadefc4ff0dd7d55db841281543693466b
zepuwang <zepu@seas.upenn.edu>
2023-04-19 19:07:28 -0400

    Update main.py

--- assignments/05-RL/main.py
+++ assignments/05-RL/main.py
@@ -18,7 +18,7 @@ agent = Agent(
 
 total_reward = 0
 last_n_rewards = []
-for _ in range(1):
+for _ in range(0):
     action = agent.act(observation)
     observation, reward, terminated, truncated, info = env.step(action)
     agent.learn(observation, reward, terminated, truncated)
