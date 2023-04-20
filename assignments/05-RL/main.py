8e3f558f8def3712d88aa87045a0e8a4556bc89b
zepuwang <zepu@seas.upenn.edu>
2023-04-20 15:23:25 -0400

    Update main.py

--- assignments/05-RL/main.py
+++ assignments/05-RL/main.py
@@ -11,6 +11,34 @@ SHOW_ANIMATIONS = True
 env = gym.make("LunarLander-v2", render_mode="human" if SHOW_ANIMATIONS else "none")
 observation, info = env.reset(seed=42)
 
+agent = Agent(
+    action_space=env.action_space,
+    observation_space=env.observation_space,
+)
+
+total_reward = 0
+last_n_rewards = []
+for _ in range(100000):
+    action = agent.act(observation)
+    observation, reward, terminated, truncated, info = env.step(action)
+    agent.learn(observation, reward, terminated, truncated)
+    total_reward += reward
+
+    if terminated or truncated:
+        observation, info = env.reset()
+        last_n_rewards.append(total_reward)
+        n = min(30, len(last_n_rewards))
+        avg = sum(last_n_rewards[-n:]) / n
+        # improvement_emoji = "🔥" if (total_reward > avg) else "😢"
+        # print(
+        #   f"{improvement_emoji} Finished with reward {int(total_reward)}.\tAverage of last {n}: {int(avg)}"
+        # )
+        # if avg > 0:
+        #   print("🎉 Nice work! You're ready to submit the leaderboard! 🎉")
+        total_reward = 0
+
+env.close()
+
 
 # Import the Agent class
 
@@ -29,6 +57,7 @@ agent = Agent(
 # Train the agent for 100 episodes
 for episode in range(100):
     # Reset the environment for a new episode
+    observation = env.reset()
     done = False
     total_reward = 0
 
