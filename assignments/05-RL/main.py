2af485aafa5fadae4b11553902ffc56d1f95bfa5
zepuwang <zepu@seas.upenn.edu>
2023-04-20 15:25:34 -0400

    Update main.py

--- assignments/05-RL/main.py
+++ assignments/05-RL/main.py
@@ -40,42 +40,7 @@ for _ in range(100000):
 env.close()
 
 
-# Import the Agent class
 
 
-# Create the CartPole-v1 environment
-env = gym.make("CartPole-v1")
 
-# Create an instance of the Agent class
-agent = Agent(
-    action_space=env.action_space,
-    observation_space=env.observation_space,
-    learning_rate=0.01,
-    discount_factor=0.99,
-)
 
-# Train the agent for 100 episodes
-for episode in range(100):
-    # Reset the environment for a new episode
-    observation = env.reset()
-    done = False
-    total_reward = 0
-
-    while not done:
-        # Select an action using the agent's policy
-        action = agent.act(observation)
-
-        # Take a step in the environment using the selected action
-        next_observation, reward, done, info = env.step(action)
-
-        # Update the agent's policy based on the observed transition
-        agent.learn(observation, action, reward, next_observation, done)
-
-        # Update the current observation and reward
-        observation = next_observation
-        total_reward += reward
-
-    print(f"Episode {episode}: Total reward = {total_reward}")
-
-# Close the environment
-env.close()
